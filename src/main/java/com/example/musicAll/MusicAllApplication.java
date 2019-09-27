a56569a2427ba788ebb19bea03e91f4c7ea0ecb4
judycodes <43575215+judycodes@users.noreply.github.com>
2019-09-27 18:11:47 -0400

    app name changed

--- src/main/java/com/example/musicAll/MusicAllApplication.java
+++ src/main/java/com/example/musicAll/MusicAllApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MusicAllApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MusicAllApplication.class, args);
 	}
 
 }
