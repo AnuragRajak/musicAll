455ce6dcc8560eff9e018906202e274009682283
BenKats <katsbenjamin@gmail.com>
2019-09-29 23:24:36 -0400

    Created Artist table without relational mapping

---
+++ src/main/java/com/example/musicAll/Model/Artist.java
@@ -0,0 +1,33 @@
+package com.example.musicAll.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "artists")
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+    public Artist(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
